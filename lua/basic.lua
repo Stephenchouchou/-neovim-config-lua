0d847ab1b7037b50d1fd8bae10aa660c92c6eec5
chienchou.lin <chienchou.lin@vertexcom.com>
2022-12-19 14:43:57 +0800

    [add]enable Hybird line numbers

--- lua/basic.lua
+++ lua/basic.lua
@@ -1,1 +1,12 @@
-vim.wo.number = true
+-- vim.g.{name}    :golbal variable
+-- vim.b.{name}    :buffer variable
+-- vim.w.{name}    :windows variable
+-- vim.bo.{option} :buffer-local domain
+-- vim.wo.{option} :windows-local domain
+
+-- Enable Hybrid line numbers by setting below two option
+vim.wo.number = true         -- 開啟行號
+vim.wo.relativenumber = true -- 開啟相對行號
+
+
+
